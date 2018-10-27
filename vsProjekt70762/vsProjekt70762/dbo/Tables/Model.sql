30887adf16829a732301dab5d987d205d7e1297d
Michal <m.rzechowicz@gmail.com>
2018-10-27 19:34:31 +0200

    klucze obce

--- vsProjekt70762/vsProjekt70762/dbo/Tables/Model.sql
+++ vsProjekt70762/vsProjekt70762/dbo/Tables/Model.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[Models] (
+﻿CREATE TABLE [dbo].[Model] (
     [Mod_Id]                 INT             IDENTITY (1, 1) NOT NULL,
     [Mod_Code]               VARCHAR (20)    NOT NULL,
     [Mod_Name]               VARCHAR (100)   NOT NULL,
@@ -13,6 +13,4 @@
     CONSTRAINT [PK_ModId] PRIMARY KEY CLUSTERED ([Mod_Id] ASC),
     CONSTRAINT [UQ_ModCode] UNIQUE NONCLUSTERED ([Mod_Code] ASC)
 );
-
-
 
--- vsProjekt70762/vsProjekt70762/dbo/Tables/Orders.sql
+++ vsProjekt70762/vsProjekt70762/dbo/Tables/Orders.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Orders] (
     [Ord_Id]                INT          IDENTITY (1, 1) NOT NULL,
     [Ord_Code]              VARCHAR (12) NOT NULL,
-    [Ord_CustId]            INT          NOT NULL,
+    [Ord_CusId]             INT          NOT NULL,
     [Ord_EmpId]             INT          NOT NULL,
     [Ord_ModId]             INT          NOT NULL,
     [Ord_MprId]             INT          NOT NULL,
@@ -12,6 +12,8 @@
     CONSTRAINT [PK_OrdId] PRIMARY KEY CLUSTERED ([Ord_Id] ASC),
     CONSTRAINT [UQ_OrdCode] UNIQUE NONCLUSTERED ([Ord_Code] ASC)
 );
+
+
 
 
 
